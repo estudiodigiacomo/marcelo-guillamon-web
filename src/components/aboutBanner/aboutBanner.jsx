a7a134ae650f756ba2a4e2bf8778a499b1b0d822
bruno <bruno@estudiodigiacomo.com>
2025-01-17 13:37:52 +0000

    finish about

--- src/components/aboutBanner/aboutBanner.jsx
+++ src/components/aboutBanner/aboutBanner.jsx
@@ -11,22 +11,28 @@ const aboutBanner = () => {
         </div>
         <div className="first-text">
             <p className='subtile-about'>¿Quienes Somos?</p>
-            <p className='text-content'>En Marcelo Guillamón Automotores, somos más que una concesionaria de autos en Tres Arroyos; somos un equipo que comparte con nuestros clientes la pasión y la confianza que representa Ford. Nos dedicamos a ofrecer una experiencia completa y personalizada en la adquisición de vehículos, brindando asesoramiento profesional y una atención cálida y cercana. Nuestra meta es que cada cliente se sienta parte de nuestra historia y encuentre en nosotros el respaldo que necesita para elegir el vehículo ideal.</p>
+            <p className='text-content'>Marcelo Guillamón Automotores (MGA) es un apéndice, de una gran empresa, Vicente Guillamón S.A. (VGSA) concesionaria Ford por 50 años en Tres Arroyos. También representamos a John Deere y Volkswagen.</p>
+            <p className='text-content'>Soy Marcelo Guillamón, nieto del fundador y titular de MGA.</p>
+            <p className='text-content'>Me formé laboralmente en VGSA, bajo sus principios y valores.</p>
+            <p className='text-content'>En 2013 comenzó la etapa de MGA.</p>
+            <p className='text-content'>Son los mismos principios y valores quienes guían nuestra empresa y a las personas que la formamos.</p>
+            <p className='text-content'>El Propósito y Compromiso es generar Bienestar para todos los involucrados en nuestra actividad: Directivos, Personal, Clientes y Proveedores.</p>
+            <p className='text-content'><b>Generar, mantener y recuperar clientes</b>, en caso de pérdida, <b>es nuestro norte.</b></p>
+            <p className='text-content'>Generar la Confianza de cumplir con lo prometido es el instrumento para lograrlo.</p>
+            <p className='text-content'>Una buena cartera de clientes nos asegura crecer y ofrecerles más valor.</p>
+            <p className='text-content'>Contar con clientes que nos valoren, contribuye a nuestro propósito de bienestar.</p>
+            <p className='text-content'>Eleva nuestra autoestima si nos sentimos miembros de una organización Prestigiosa.</p>
+            <p className='text-content'>Igual que mis antecesores, yo también elegí:</p>
         </div>
-        <div className="column-text">
-            <div className="first-text">
-                <p className='subtile-about'>Nuestra Historia</p>
-                <p className='text-content'>Con décadas de trayectoria en la industria automotriz, Marcelo Guillamón Automotores ha recorrido un camino lleno de compromiso y dedicación hacia la comunidad. En cada uno de nuestros años de servicio, hemos llevado adelante el legado de excelencia y calidad que caracteriza a Ford, creciendo y evolucionando para satisfacer las necesidades de nuestros clientes. Nuestra historia está marcada por la confianza y el respeto mutuo, siendo siempre un punto de referencia en Tres Arroyos y la región.</p>
-            </div>
-            <div className="first-text">
-                <p className='subtile-about'>Nuestra Misión</p>
-                <p className='text-content'>Nos impulsa el compromiso de acercar a cada cliente un vehículo que represente seguridad, innovación y estilo. Nuestra misión es ofrecer una atención personalizada que no solo se centre en la venta, sino en crear una relación duradera basada en la satisfacción y la confianza. Queremos ser parte de cada historia, de cada viaje, y de cada aventura que nuestros clientes emprenden, acompañándolos en cada kilómetro recorrido.</p>
-            </div>
+        <h2 className='lema-text'>"Una Vida con Ford"</h2>
+        <div className="first-text">
+            <p className='text-content'>Atentamente,</p>
+            <p className='text-content'>Marcelo Gullamón</p>
         </div>
         <div class="image-gallery">
             <img src="https://storage.googleapis.com/guillamon-img/IMG_20241024_110518243_HDR.jpg" alt="Imagen 1" className="gallery-image" />
             <img src="https://storage.googleapis.com/guillamon-img/IMG_20241024_110948076_HDR.jpg" alt="Imagen 2" className="gallery-image" />
-            <img src="https://storage.googleapis.com/guillamon-img/entrega.jpg" alt="Imagen 3" className="gallery-image" />
+            <img src="https://storage.googleapis.com/guillamon-img/IMG_20241220_094544045_HDR.jpg" alt="Imagen 3" className="gallery-image" />
         </div>
         <div className="about-cards-container">
             <div className="about-card">
